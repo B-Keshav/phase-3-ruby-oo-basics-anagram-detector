d74a9b7d84d7b0a635cc120728a0cb8bb2b2bcfc
Keshav Bhati <keshavbhati2000@gmail.com>
2022-11-01 13:14:31 -0400

    commit tree 5febe6bdc6160535c59072a3509ac03ed1e79e8e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter do |array_word|
+            @word.chars.sort == array_word.chars.sort
+        end
+    end
+
+end
